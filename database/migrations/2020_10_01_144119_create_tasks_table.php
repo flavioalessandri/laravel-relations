ce1dbc9d2d8eb119fedceb16ef08713ccd216c6c
Flavio Alessandri <flavio.alessandri@gmail.com>
2020-10-01 17:22:05 +0200

    Migrations created

---
+++ database/migrations/2020_10_01_144119_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 50);
+            $table->string('description',250);
+            $table->integer('difficulty_level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
